2b443b793cdaf294d7f1b0efa2028b134898434f
nazar <nhavronskyi@gmail.com>
2023-07-10 21:58:12 +0200

    simple config

---
+++ src/main/java/com/example/spotikconfigservice/SpotikConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.spotikconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpotikConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotikConfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spotikconfigservice/SpotikConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotikconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotikConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
